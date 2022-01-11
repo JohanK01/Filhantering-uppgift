e39a6aeeef0a561e3df6eb1b67547ac84faba1b7
JohanK <klausenjohan23@gmail.com>
2022-01-11 12:00:20 +0100

    ready to go

---
+++ Filhantering/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Filhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string Name = Console.ReadLine();
+            Console.WriteLine(Name);
+        }
+    }
+}
